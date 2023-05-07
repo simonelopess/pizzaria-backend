89e2ba1eb68f1f9db1b840986b3d7a969fd37138
Simone Lopes <sisilopes20@gmail.com>
2023-05-07 10:50:09 +0100

    configurando envio da imagem do produto

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/products/CreateProductController';
+
+import uploadConfig from './config/multer';
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"));
 
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
@@ -14,5 +20,7 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export { router };
